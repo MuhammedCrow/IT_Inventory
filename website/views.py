d913b9ebb194095dc6546a8b3a1e3b70c665102f
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-06-09 15:56:32 +0300

    update

--- website/views.py
+++ website/views.py
@@ -6,3 +6,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+
+@views.route('/computers')
+def computers():
+    return render_template("computers.html")
