8c0b561a43023a2a4557fae78be7b6f7d00367f4
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-06-26 15:38:47 +0300

    some python updates

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>login</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
