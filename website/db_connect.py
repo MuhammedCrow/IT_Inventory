4949d374dce74e8f99b33e052b96f9f5e9b6afd5
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-09-09 15:40:49 +0300

    test

--- website/db_connect.py
+++ website/db_connect.py
@@ -5,7 +5,7 @@ def connect_sql():
     connStr = (
         r'DRIVER={SQL Server};'
         r'SERVER=KCML-LT239;'
-        r'DATABASE=master;'
+        r'DATABASE=inventory;'
         r'UID=malaa;'
         r'PWD=P@ssw0rd;'
     )
