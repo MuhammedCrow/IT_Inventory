59193a21cec3b97a1da67f35934ffa9c59e57cbd
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-09-22 15:13:59 +0300

    lots of edits

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from werkzeug.utils import redirect
 
 views = Blueprint('views', __name__)
 
@@ -30,15 +31,54 @@ def network():
 
 @views.route('/cartridges')
 def cartridges():
-    return render_template("cartridges.html")
+    from .db_connect import connect_sql
+    conx = connect_sql()
+    query = 'select name from consumable where makeAndModle = 1'
+    cursor = conx.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return render_template("cartridges.html", data=data)
 
 
 @views.route('/addCon', methods=['GET', 'POST'])
 def addCon():
+    amount = int(request.form.get('amount'))
+    model = request.form.get('model')
     from .db_connect import connect_sql
     conx = connect_sql()
-    query = 'INSERT into dbo.consumable (name, categoryId, amount, makeAndModelId) values (?, ?, ?, ?)'
+    query = 'update dbo.consumable SET amount += ? where dbo.consumable.name = ?'
     cursor = conx.cursor()
-    cursor.execute(query, 'test', 1, 10, 1)
-    conx.comit()
+    cursor.execute(query, amount, model)
+    conx.commit()
     conx.close()
+    return redirect('/cartridges')
+
+
+@views.route('/issueCon', methods=['GET', 'POST'])
+def issueCon():
+    amount = int(request.form.get('amount'))
+    model = request.form.get('model')
+    date = request.form.get('date')
+    from .db_connect import connect_sql
+    conx = connect_sql()
+    query = 'update dbo.consumable SET amount -= ? where dbo.consumable.name = ?'
+    cursor = conx.cursor()
+    cursor.execute(query, amount, model)
+    conx.commit()
+    conx.close()
+    return redirect('/cartridges')
+
+
+@views.route('/addComputer', methods=['GET', 'POST'])
+def addComputer():
+    snumber = request.form.get('snumber')
+    model = request.form.get('model')
+    condition = request.form.get('condition')
+    from .db_connect import connect_sql
+    conx = connect_sql()
+    query = 'insert into hardware(serialNumber, categoryId, makeAndModel,'
+    cursor = conx.cursor()
+    cursor.execute(query, amount, model)
+    conx.commit()
+    conx.close()
+    return redirect('/cartridges')
