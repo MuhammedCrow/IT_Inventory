18cdb411b8294927555eae3efd2c398e91e21e97
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-11-28 16:14:58 +0300

    sss

--- website/db_connect.py
+++ website/db_connect.py
@@ -4,7 +4,7 @@ import pyodbc
 def connect_sql():
     connStr = (
         r'DRIVER={SQL Server};'
-        r'SERVER=DESKTOP-N60GPF0;'
+        r'SERVER=KCML-LT239;'
         r'DATABASE=inventory;'
         r'UID=malaa;'
         r'PWD=P@ssw0rd;'
