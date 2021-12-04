211fec011bfa833a715cc62fac4c39d0f2ab370a
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-12-04 22:49:00 +0300

    reports

--- website/reports.py
+++ website/reports.py
@@ -6,3 +6,17 @@ from .db_connect import connect_sql
 
 
 reports = Blueprint('reports', __name__)
+
+from .views import fetchDepartments
+reports.route('/cartridgeCons')
+def cartridgeCons():
+    depts = fetchDepartments()
+    headings = ('Cartridge', 'amount')
+    return render_template('consumption.html', depts=depts, headings=headings)
+
+reports.route('/searchDeptCons', methods=['POST'])
+def searchDeptCons():
+    dept = request.form.get('dept')
+    startDate = request.form.get('startDate')
+    endDate = request.form.get('endDate')
+    query = 'SELECT '
