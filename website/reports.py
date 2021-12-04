d98a6fc23f394469f6ce70cf522815b5a4e15cf8
Muhammed Aladin <44171554+MuhammedCrow@users.noreply.github.com>
2021-12-04 23:29:14 +0300

    consumption report done

--- website/reports.py
+++ website/reports.py
@@ -1,3 +1,4 @@
+from .views import fetchDepartments
 from flask import Blueprint, render_template, request, jsonify, flash
 from flask.globals import session
 from flask.helpers import url_for
@@ -7,16 +8,34 @@ from .db_connect import connect_sql
 
 reports = Blueprint('reports', __name__)
 
-from .views import fetchDepartments
 reports.route('/cartridgeCons')
+
+
 def cartridgeCons():
     depts = fetchDepartments()
-    headings = ('Cartridge', 'amount')
-    return render_template('consumption.html', depts=depts, headings=headings)
+    return render_template('consumption.html', depts=depts)
+
 
 reports.route('/searchDeptCons', methods=['POST'])
+
+
 def searchDeptCons():
+    headings = ('Cartridge', 'amount')
     dept = request.form.get('dept')
     startDate = request.form.get('startDate')
     endDate = request.form.get('endDate')
-    query = 'SELECT '
+    getDeptId = 'select id from departments where name = ?'
+    query = 'SELECT consumable.name, sum(consConsumption.amount) as amount from consConsumption INNER JOIN consumable on consConsumption.consId = consumable.id WHERE consConsumption.date >= \'?\' AND consConsumption.date < \'?\' AND consConsumption.deptId = ? AND consConsumption.catId = ? GROUP BY consumable.name'
+    try:
+        from .db_connect import connect_sql
+        conx = connect_sql()
+        cursor = conx.cursor()
+        cursor.execute(getDeptId, dept)
+        deptId = cursor.fetchone()
+        cursor.execute(query, startDate, endDate, deptId, 1)
+        data = cursor.fetchall()
+        conx.close()
+        return render_template('consumption.html', headings=headings, data=data)
+    except Exception as e:
+        flash(str(e), category='error')
+    return render_template('consumption.html', headings=headings)
